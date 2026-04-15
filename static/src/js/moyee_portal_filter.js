a45e354b33393d9857e3d3c3cc96c683bfe8b1dc
Ali Hassan <156808621+devhassan17@users.noreply.github.com>
2026-04-16 01:02:39 +0500

    refactor: remove price filter functionality and update portal section styling themes

--- static/src/js/moyee_portal_filter.js
+++ static/src/js/moyee_portal_filter.js
@@ -6,7 +6,6 @@ publicWidget.registry.MoyeeProductFilter = publicWidget.Widget.extend({
     selector: ".o_portal_wrap",
     events: {
         'change .moyee-filter-check': '_onFilterChange',
-        'input .moyee-price-slider': '_onPriceSliderChange',
         'click #clearMoyeeFilters': '_onClearFilters',
     },
 
@@ -17,9 +16,6 @@ publicWidget.registry.MoyeeProductFilter = publicWidget.Widget.extend({
         this.$cards = this.$(".js_moyee_product_card");
         this.$noResults = this.$("#moyeeNoResults");
         this.$slider = this.$("#moyeeProductSlider");
-        this.$priceMinLabel = this.$("#priceMinLabel");
-        this.$priceMaxLabel = this.$("#priceMaxLabel");
-        this.$priceRange = this.$("#priceRange");
 
         return this._super.apply(this, arguments);
     },
@@ -32,18 +28,9 @@ publicWidget.registry.MoyeeProductFilter = publicWidget.Widget.extend({
         this._applyFilters();
     },
 
-    _onPriceSliderChange: function (ev) {
-        const val = parseFloat($(ev.currentTarget).val());
-        this.$priceMaxLabel.text(val.toFixed(2));
-        this._applyFilters();
-    },
-
     _onClearFilters: function (ev) {
         ev.preventDefault();
         this.$(".moyee-filter-check").prop("checked", false);
-        const maxPrice = this.$priceRange.attr("max");
-        this.$priceRange.val(maxPrice);
-        this.$priceMaxLabel.text(parseFloat(maxPrice).toFixed(2));
         this._applyFilters();
     },
 
@@ -54,7 +41,6 @@ publicWidget.registry.MoyeeProductFilter = publicWidget.Widget.extend({
     _applyFilters: function () {
         const activeGrinds = this.$(".moyee-filter-check[id^='grind']:checked").map(function() { return $(this).val(); }).get();
         const activeWeights = this.$(".moyee-filter-check[id^='weight']:checked").map(function() { return $(this).val(); }).get();
-        const maxPrice = parseFloat(this.$priceRange.val());
 
         let visibleCount = 0;
 
@@ -62,13 +48,11 @@ publicWidget.registry.MoyeeProductFilter = publicWidget.Widget.extend({
             const $card = $(this);
             const grind = $card.data("grind");
             const weight = $card.data("weight");
-            const price = parseFloat($card.data("price"));
 
             const matchGrind = activeGrinds.length === 0 || activeGrinds.includes(grind);
             const matchWeight = activeWeights.length === 0 || activeWeights.includes(weight);
-            const matchPrice = price <= maxPrice;
 
-            if (matchGrind && matchWeight && matchPrice) {
+            if (matchGrind && matchWeight) {
                 $card.removeClass("d-none");
                 visibleCount++;
             } else {
